86c89fb8bad16ec3e0f288b7c8d1b38de1271783
2021middletonmatthew <80419585+2021middletonmatthew@users.noreply.github.com>
2021-03-31 16:35:21 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -69,6 +69,17 @@ function imageUpload(){
     
     let textInput = document.getElementById('input');
 
+    
+    if(localStorage.getItem("theme" == "light")){
+		currentStyleSheet1.href = window.location.href + "css/style.css";
+		currentStyleSheet2.href = window.location.href + "css/imgur.min.css";
+		localStorage.setItem("theme", "light");
+	}else if(localStorage.getItem("theme" == "dark")){
+		currentStyleSheet1.href = window.location.href + "css/style.dark.css";
+		currentStyleSheet2.href = window.location.href + "css/imgur.min.dark.css";
+		localStorage.setItem("theme", "dark");	
+        
+        
     var output = PUBNUB.$('output'), 
         input = PUBNUB.$('input'), 
         button = PUBNUB.$('button'),
