8b0bc16e6818ff3f96f450a61d2025e6c9be8225
2021middletonmatthew <80419585+2021middletonmatthew@users.noreply.github.com>
2021-04-23 13:42:56 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,69 +1,57 @@
+let textInput = document.getElementById('input');
 
+var output = PUBNUB.$('output'), 
+input = PUBNUB.$('input'), 
+button = PUBNUB.$('button'),
+avatar = PUBNUB.$('avatar')
 
-(function() {
-
-
-    let textInput = document.getElementById('input');
-
+var channel = 'mchat';
     
-
-    var output = PUBNUB.$('output'), 
-        input = PUBNUB.$('input'), 
-        button = PUBNUB.$('button'),
-        avatar = PUBNUB.$('avatar')
-
-    var channel = 'mchat';
+// Assign a random avatar in random color
     
-    // Assign a random avatar in random color
-    
-    let avatarChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    let colorChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let avatarChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let colorChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	
 
-    let trollFaceNum = Math.floor(Math.random() * 100);
-    console.log(trollFaceNum);
-    if(trollFaceNum == 69){
-        avatar.className = "trollFace";
-    }else{
-	avatarChoice = avatarChoices[((Math.random() * 13 + 1) >>> 0)-1];
-	avatarColor = colorChoices[((Math.random() * 10 + 1) >>> 0)-1];
-        avatar.className = 'face-' + String(avatarChoice) + ' color-' + String(avatarColor);
-    }
+let trollFaceNum = Math.floor(Math.random() * 100);
+console.log(trollFaceNum);
+if(trollFaceNum == 69){
+    avatar.className = "trollFace";
+}else{
+avatarChoice = avatarChoices[((Math.random() * 13 + 1) >>> 0)-1];
+avatarColor = colorChoices[((Math.random() * 10 + 1) >>> 0)-1];
+    avatar.className = 'face-' + String(avatarChoice) + ' color-' + String(avatarColor);
+}
 
-    var p = PUBNUB.init({
-        subscribe_key: 'sub-c-1d799376-85b5-11eb-a47e-8aa5932e3236',
-        publish_key:   'pub-c-c3564aa2-a04d-4ab0-bed3-93a42c77e870'
-    });
+var p = PUBNUB.init({
+    subscribe_key: 'sub-c-1d799376-85b5-11eb-a47e-8aa5932e3236',
+    publish_key:   'pub-c-c3564aa2-a04d-4ab0-bed3-93a42c77e870'
+});
 
-    p.subscribe({
-        channel  : channel,
-        callback : function(m) {
-            if(m.href == undefined){
-                output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' +  m.text.replace( /[<>]/ig, '' ) + '</span></p>' + output.innerHTML;
-            }
+p.subscribe({
+    channel  : channel,
+    callback : function(m) {
+        if(m.href == undefined){
+            output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' +  m.text.replace( /[<>]/ig, '' ) + '</span></p>' + output.innerHTML;
         }
-    });
-
-    p.bind('keyup', input, function(e) {
-        (e.keyCode || e.charCode) === 13 && publish()
-    });
-
-    p.bind('click', button, publish);
-
-    function publish() {
-        if(textInput.value.length > 0){
-            p.publish({
-                channel : channel, 
-                message : {avatar: avatar.className, text: input.value}, 
-                x : (input.value='')
-            });
-        };
     }
+});
 
+p.bind('keyup', input, function(e) {
+    (e.keyCode || e.charCode) === 13 && publish()
+});
 
-    
+p.bind('click', button, publish);
 
-})();
+function publish() {
+    if(textInput.value.length > 0){
+        p.publish({
+            channel : channel, 
+            message : {avatar: avatar.className, text: input.value}, 
+            x : (input.value='')
+        });
+    };
+}
 
 var linkValue;
 var currentStyleSheet1 = document.getElementById('styleSheet1');
@@ -130,4 +118,4 @@ function imageUpload(){
         clientid: '7f0647e1e9365c2',
         callback: callbackLink
     });
-};
+};  
